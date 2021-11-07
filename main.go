130e7ba3ece3b07ebecad79c41d3eb0768c995d3
Kevin <void@arpa.gov>
2021-11-07 00:53:28 -0400

    add multiple domain list

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func HandleErr(err error) {
 	}
 }
 
+func RemoveIndex(slice []string, s int) []string {
+    return append(slice[:s], slice[s+1:]...)
+}
+
 func addToCloudflare(apiKey string, domain string, dnsRecords string) error {
 	id, err := GetAccountId(apiKey)
 	if err != nil {
@@ -46,6 +50,7 @@ func main() {
 	cloudflareApiKey := flag.String("cloudflareSecret", "", "Cloudflare API key.")
 	dnsRecordsFile := flag.String("dnsRecordsFile", "", "Filename containing DNS records.")
 	domainsFile := flag.String("domainsFile", "", "Filename containing a newline deliminated list of domains to register.")
+	lowBal := flag.Float64("lowBalanceLimit", 0, "If balance is below this number, don't register more names.")
 
 	ns1 := flag.String("ns1", "", "Cloudflare nameserver 1.")
 	ns2 := flag.String("ns2", "", "Cloudflare nameserver 2.")
@@ -60,30 +65,13 @@ func main() {
 		log.Fatal("Please specify a DNS records file with --dnsRecordsFile.")
 	}
 
+	if *domainsFile == "" {
+		log.Fatal("Please specify a domain list file with --domainsFile.")
+	}
+
 	if *ns1 == "" || *ns2 == "" {
 		log.Fatal("Please specify your Cloudflare nameservers with --ns1 and --ns2.")
 	}
-
-	domain := "jeffersonaeroplane.xyz"
-
-	var err error
-	Balance, err = GetBalance(*namesiloApiKey)
-	HandleErr(err)
-
-	log.Printf("Balance is $%f\n", Balance)
-	log.Printf("Buying domain %s\n", domain)
-
-	if Balance > 10 {
-		err = RegisterDomain(*namesiloApiKey, domain, *ns1, *ns2)
-		HandleErr(err)
-	} else {
-		log.Fatal("Low balance")
-	}
-
-	Balance, err = GetBalance(*namesiloApiKey)
-	HandleErr(err)
-
-	log.Printf("Balance is $%f\n", Balance)
 
 	f, err := os.Open(*dnsRecordsFile)
 	HandleErr(err)
@@ -91,6 +79,34 @@ func main() {
 	dnsRecords, err := ioutil.ReadAll(f)
 	HandleErr(err)
 
-	err = addToCloudflare(*cloudflareApiKey, domain, string(dnsRecords))
+	f, err = os.Open(*domainsFile)
 	HandleErr(err)
+
+	domainsList, err := ioutil.ReadAll(f)
+	HandleErr(err)
+
+	domains := strings.Split(string(domainsList), "\n")
+
+	for i, domain := range domains {
+		Balance, err = GetBalance(*namesiloApiKey)
+		HandleErr(err)
+
+		log.Printf("Balance is $%f\n", Balance)
+
+		if Balance > *lowBal {
+			log.Printf("Buying domain %s\n", domain)
+			err = RegisterDomain(*namesiloApiKey, domain, *ns1, *ns2)
+			if err != nil {
+				log.Println(err)
+				domains = RemoveIndex(domains, i)
+			}
+		} else {
+			log.Fatal("Low balance")
+		}
+	}
+
+	for _, domain := range domains {
+		err = addToCloudflare(*cloudflareApiKey, domain, string(dnsRecords))
+		HandleErr(err)
+	}
 }
